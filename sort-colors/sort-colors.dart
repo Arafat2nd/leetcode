bba2f7cce52fcbf4ad9a56dcda585df85a16e6ba
Arafat2nd <Sohaib.arafat@outlook.com>
2023-06-19 14:07:02 +0300

    Time: 264 ms (64.29%) | Memory: 141.8 MB (92.86%) - LeetSync

--- sort-colors/sort-colors.dart
+++ sort-colors/sort-colors.dart
@@ -1,6 +1,14 @@
 class Solution {
   void sortColors(List<int> nums) {
-nums.sort();
+  for (int i = 1; i < nums.length; i++) {
+    int current = nums[i];
+    int j = i - 1;
+    while (j >= 0 && nums[j] > current) {
+      nums[j + 1] = nums[j];
+      j--;
+    }
+    nums[j + 1] = current;
+  }
  
   }
 }
