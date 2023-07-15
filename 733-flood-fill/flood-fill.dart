a6431d8e4754cc505e50f443a4fb74ae13369af1
Arafat2nd <Sohaib.arafat@outlook.com>
2023-07-15 23:28:32 +0300

    Time: 333 ms (18.75%) | Memory: 145.7 MB (6.25%) - LeetSync

--- 733-flood-fill/flood-fill.dart
+++ 733-flood-fill/flood-fill.dart
@@ -4,21 +4,19 @@ class Solution {
         int cols = image[0].length;
         int oldColor = image[sr][sc];
         
-        floodFillUtil(image, sr, sc, color, oldColor, rows, cols);
+        floodFillRec(image, sr, sc, color, oldColor, rows, cols);
         
         return image;
     }
     
-    void floodFillUtil(List<List<int>> image, int sr, int sc, int newColor, int oldColor, int rows, int cols) {
+    void floodFillRec(List<List<int>> image, int sr, int sc, int newColor, int oldColor, int rows, int cols) {
         if (sr < 0 || sr >= rows || sc < 0 || sc >= cols || image[sr][sc] != oldColor || image[sr][sc] == newColor) {
             return;
-        }
-        
-        image[sr][sc] = newColor;
-        
-        floodFillUtil(image, sr - 1, sc, newColor, oldColor, rows, cols);  
-        floodFillUtil(image, sr + 1, sc, newColor, oldColor, rows, cols);  
-        floodFillUtil(image, sr, sc - 1, newColor, oldColor, rows, cols);  
-        floodFillUtil(image, sr, sc + 1, newColor, oldColor, rows, cols);  
+        }    
+        image[sr][sc] = newColor; 
+        floodFillRec(image, sr - 1, sc, newColor, oldColor, rows, cols);  
+        floodFillRec(image, sr + 1, sc, newColor, oldColor, rows, cols);  
+        floodFillRec(image, sr, sc - 1, newColor, oldColor, rows, cols);  
+        floodFillRec(image, sr, sc + 1, newColor, oldColor, rows, cols);  
     }
 }
