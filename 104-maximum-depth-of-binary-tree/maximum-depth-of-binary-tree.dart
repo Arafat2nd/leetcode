0f8a244418d49b38dabc709e0860c57edb86ea31
Arafat2nd <Sohaib.arafat@outlook.com>
2023-08-14 11:15:45 +0300

    Time: 234 ms (88.57%) | Memory: 142.2 MB (91.43%) - LeetSync

---
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.dart
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *   int val;
+ *   TreeNode? left;
+ *   TreeNode? right;
+ *   TreeNode([this.val = 0, this.left, this.right]);
+ * }
+ */
+class Solution {
+ int maxDepth(TreeNode? root) {
+  if (root == null) {
+    return 0;
+  }
+
+  int leftDepth = maxDepth(root.left);
+  int rightDepth = maxDepth(root.right);
+
+  return max(leftDepth, rightDepth) + 1;
+}
+
+}
+ 
+
